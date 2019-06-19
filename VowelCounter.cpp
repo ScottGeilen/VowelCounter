144a494efc7c40e37a18b268c53505a5122ebc78
Scottacious <scottgeilen99@gmail.com>
2019-06-19 19:05:42 -0400

    Adding VowelCounter.cpp

---
+++ VowelCounter.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare variable for user's string
+    string line;
+
+    // Declare variable to count vowels
+    int vowels;
+
+    // Initialize vowels to 0
+    vowels = 0;
+
+    // Ask user to enter a line of string
+    cout << "Enter a line of string: ";
+
+    // Capture string which has spaces
+    getline(cin, line);
+
+    // int i is 0
+    // Iterate until length of string is reached
+    // Add 1 to i
+    for (int i = 0; i < line.length(); ++i)
+    {
+        // Checks each for each vowel in the user string
+        if(line[i]=='a' || line[i]=='e' || line[i]=='i' || line[i]=='o' || line[i]=='u')
+        {
+            ++vowels;
+        }
+    }
+
+    // If there is 1 vowel in the user string
+    if(vowels == 1)
+    {
+        cout << "\n\nYou have " << vowels << " vowel in your line of string.\n\n" << endl;
+    }
+    // If there are 0 or more than 1 vowels in the user string
+    else
+    {
+        cout << "\n\nYou have " << vowels << " vowels in your line of string.\n\n" << endl;
+    }
+
+}
